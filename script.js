6d5de74a4b9d06ae8ce2eecd08e9e5a8658d2c58
Carlos Fernandez <fernandezcarlospy@hotmail.com>
2024-06-08 18:39:16 -0700

    finish, this is my portafolio 2024 <3
    Nice

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuIcon = document.getElementById('menu-icon');
-    const navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let section = document.querySelector('section');
+let navLinks = document.querySelector('header nava');
 
-    menuIcon.addEventListener('click', () => {
-        navbar.classList.toggle('active');
-    });
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute;
 
-    window.addEventListener('resize', () => {
-        if (window.innerWidth > 480) {
-            navbar.classList.remove('active');
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*='+ id +']').classList.add
+                ('active');
+            })
         }
-    });
-});
+    })
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
